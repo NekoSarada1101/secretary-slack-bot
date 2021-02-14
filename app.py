c09c4bad1be58122903a60acfd57daca8004e70c
NekoSarada1101 <ryoyukikazu11@gmail.com>
2021-02-14 18:24:47 +0900

    feat: Slackでテキストを表示する

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask
+import requests
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def hello_world():
+    data = {
+        "text": "hello_world"
+    }
+
+    # Slackでは両方とも表示された
+    json_data = json.dumps(data).encode("utf-8")
+    response = requests.post(
+        "https://hooks.slack.com/services/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",
+        json_data)
+    print(response)
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
