b14be59d9e8e9ceced6ebad23bfe23a5a6b942ed
NekoSarada1101 <ryoyukikazu11@gmail.com>
2021-02-16 20:07:23 +0900

    feat: カレンダーAPI使用時の認証
    #2

--- settings.py
+++ settings.py
@@ -1,1 +1,9 @@
+from google.oauth2 import service_account
 SLACK_WEBHOOK_URL = "SLACK_WEBHOOK_URL"
+SCOPES = [
+    'https://www.googleapis.com/auth/calendar',
+]
+SERVICE_ACCOUNT_FILE = 'credentials.json'
+
+CREDENTIALS = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE,
+                                                                    scopes=SCOPES)
